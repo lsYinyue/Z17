5732182d20414a98639d92e2cd623eda5b61b0fc
ShuaiWang <799812761@qq.com>
2018-04-21 17:34:15 +0800

    1

---
+++ ZhengSeventeenBS/Service.Library/DBService/PermissionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Z17.Core.Dtos;
+using Z17.Core.Services;
+
+namespace Service.Library
+{
+    public class PerService : ILoginService
+    {
+        public JObject LoginCheck(string username, string password)
+        {
+            PermissionService.Proxy.GetUserMenuItems(username);
+            throw new NotImplementedException();
+        }
+        public List<MenuItemDto> GetUserMenuItems(string username)
+        {
+            var ret = PermissionService.Proxy.GetUserMenuItems(username);
+            return ret;
+        }
+    }
+}
